a7381de81260b126636099a26331ebed143e2faa
zed <zedsmail@163.com>
2016-06-03 20:52:24 +0800

    --

--- config/editor.php
+++ config/editor.php
@@ -8,5 +8,5 @@ return [
      * 在添加链接的时候可以给定一个ajax链接，这样可以方便的添加在系统中的文章
      * 服务器返回的数据格式为：{'title':'url','title2':'url2'}
      */
-//    'ajaxTopicSearchUrl' => 'topic/ajax-topics'
+    'ajaxTopicSearchUrl' => 'topic/ajax-topics'
 ];
